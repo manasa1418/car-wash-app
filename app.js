f90edc683fb5da126630f3daf172c5125d2dbadb
Your Manasa2127 <shivaramadevi55@gmail.com>
2024-09-04 20:14:40 -0500

    Resolved merge conflicts

---
+++ app.js
@@ -0,0 +1,28 @@
+let manasaReddy = ()=> {
+    let h1Tag = document.querySelector('#Manasa');
+    h1Tag.innerText ='Good Morning';
+    h1Tag.style.backgroundColor ='blue';
+    h1Tag.style.padding = '10px';
+    h1Tag.style.color = 'white';
+}
+let pavanReddy = ()=> {
+    let h1Tag = document.querySelector('#Manasa');
+    h1Tag.innerText ='Good Afternoon';
+    h1Tag.style.backgroundColor ='purple';
+    h1Tag.style.padding = '10px';
+    h1Tag.style.color = 'white';
+}
+let sivaReddy = ()=> {
+    let h1Tag = document.querySelector('#Manasa');
+    h1Tag.innerText ='Good evening';
+    h1Tag.style.backgroundColor ='red';
+    h1Tag.style.padding = '10px';
+    h1Tag.style.color = 'white';
+}
+let ramaReddy = ()=> {
+    let h1Tag = document.querySelector('#Manasa');
+    h1Tag.innerText ='Good Night';
+    h1Tag.style.backgroundColor ='yellow';
+    h1Tag.style.padding = '10px';
+    h1Tag.style.color = 'black';
+}
