c8b9aafaadc8e2056c85c25374fd0893baecf62f
Your Manasa2127 <shivaramadevi55@gmail.com>
2024-09-05 13:23:51 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Welcome to Car Wash Service</h1>
+            <p>Clean your car, clean your day!</p>
+        </header>
+    );
+}
+
+export default Header;
