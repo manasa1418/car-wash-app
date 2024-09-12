7ee6012463a01f162495f1209322498f716191a8
Your Manasa2127 <shivaramadevi55@gmail.com>
2024-09-12 17:39:45 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-let manasaReddy = ()=> {
-    let h1Tag = document.querySelector('#Manasa');
-    h1Tag.innerText ='Good Morning';
-    h1Tag.style.backgroundColor ='blue';
-    h1Tag.style.padding = '10px';
-    h1Tag.style.color = 'white';
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import BookingForm from './components/BookingForm';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <BookingForm />
+        </div>
+    );
 }
-let pavanReddy = ()=> {
-    let h1Tag = document.querySelector('#Manasa');
-    h1Tag.innerText ='Good Afternoon';
-    h1Tag.style.backgroundColor ='purple';
-    h1Tag.style.padding = '10px';
-    h1Tag.style.color = 'white';
-}
-let sivaReddy = ()=> {
-    let h1Tag = document.querySelector('#Manasa');
-    h1Tag.innerText ='Good evening';
-    h1Tag.style.backgroundColor ='red';
-    h1Tag.style.padding = '10px';
-    h1Tag.style.color = 'white';
-}
-let ramaReddy = ()=> {
-    let h1Tag = document.querySelector('#Manasa');
-    h1Tag.innerText ='Good Night';
-    h1Tag.style.backgroundColor ='yellow';
-    h1Tag.style.padding = '10px';
-    h1Tag.style.color = 'black';
-}
+
+export default App;
